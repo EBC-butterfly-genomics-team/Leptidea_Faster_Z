45b0aa0d4d32459e3cf6ba79a6116694379d2328
Lars Höök <lars.hook@ebc.uu.se>
2023-04-26 18:52:07 +0200

    Update a_trimgalore_run.sh

--- SCRIPTS/SNPS/READ_TRIMMING/a_trimgalore_run.sh
+++ SCRIPTS/SNPS/READ_TRIMMING/a_trimgalore_run.sh
@@ -1,17 +1,25 @@
 #!bin/bash
 
 
+for sample in 101C 102C 1C 2C 31C 32C 61C 62C 91C 92C
+
+do 
+  sbatch trimgalore_query.sh Swe-sin $sample
+  sleep 5
+done
+
+
+for sample in 1C 21C 22C 2C 41C 42C 61C 62C 81C 82C
+
+do
+  sbatch trimgalore_query.sh Ire-juv $sample
+  sleep 5
+done
+
+
 cd /home/larshook/LarsH/FastZ/
 mkdir -p Snps
 cd Snps
 mkdir -p Read_trimming
 cd Read_trimming
 mkdir -p REPORTS
-
-
-for sample in 101C 102C 1C 2C 31C 32C 61C 62C 91C 92C
-
-do 
-  sbatch trimgalore_query.sh $sample
-  sleep 5
-done
