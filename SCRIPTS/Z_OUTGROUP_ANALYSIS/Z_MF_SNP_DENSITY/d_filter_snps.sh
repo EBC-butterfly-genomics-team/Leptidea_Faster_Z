210aa452e7b7053d6c59a7b62e054c8e4382063d
Lars Höök <lars.hook@ebc.uu.se>
2023-11-22 11:43:05 +0100

    Update d_filter_snps.sh

--- SCRIPTS/Z_OUTGROUP_ANALYSIS/Z_MF_SNP_DENSITY/d_filter_snps.sh
+++ SCRIPTS/Z_OUTGROUP_ANALYSIS/Z_MF_SNP_DENSITY/d_filter_snps.sh
@@ -14,7 +14,7 @@ module load python/3.8.7
 cd /home/larshook/LarsH/FastZ/LEPTIDEA_OUTGROUPS/SNPS/MPILEUP
 
 
-for sample in P14502_103 P14502_104 P14458_107_S28 P14458_108_S29 P14458_103_S24 P14458_104_S25
+for sample in P14502_103 P14502_104 P14458_107_S28 P14458_108_S29 P14458_103_S24 P14458_104_S25 P14502_105 P14502_106
 do
   bcftools filter \
         -i 'GT="het" && QUAL>30 && sum(DP4)>25 && sum(DP4)<200 && (DP4[0]+DP4[1])>10 && (DP4[2]+DP4[3])>10' \
